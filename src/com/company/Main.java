eaeb0271fe0edb0a2e11db603619130aa9b03705
F-elixFelicis <f_elixfelicis@mail.ru>
2022-03-26 09:21:11 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList list = new ArrayList();
         Zoo zoo = new Zoo();
         zoo.addAnimals("Bob", 4, "Male", zoo.dog);
+        zoo.addAnimals("Pipa", 9, "Male", zoo.bird);
         zoo.addAnimals("Tom", 7, "Male", zoo.dog);
         zoo.addAnimals("Felicia", 1, "Female", zoo.bird);
         zoo.viewAnimals();
