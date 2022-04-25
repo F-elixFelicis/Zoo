af0c65479905979b419ef595af271205853bf3ad
F-elixFelicis <f_elixfelicis@mail.ru>
2022-04-25 21:14:43 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         Zoo zoo = new Zoo();
-        zoo.addAnimal("Bob", 4, Gender.Male, TypeAnimal.Dog);
-        zoo.addAnimal("Pippa", 9, Gender.Male, TypeAnimal.Bird);
-        zoo.addAnimal("Tom", 7, Gender.Male, TypeAnimal.Dog);
-        zoo.addAnimal("Felicia", 1, Gender.Male, TypeAnimal.Bird);
-        zoo.viewAnimal();
-        zoo.deleteAnimal("Tom");
-        zoo.viewAnimal();
+        zoo.addAnimals("Bob", 4, "Male", zoo.dog);
+        zoo.addAnimals("Pipa", 9, "Male", zoo.bird);
+        zoo.addAnimals("Tom", 7, "Male", zoo.dog);
+        zoo.addAnimals("Felicia", 1, "Female", zoo.bird);
+        zoo.viewAnimals();
+        zoo.deleteAnimals("Tom");
+        zoo.viewAnimals();
 
     }
 
